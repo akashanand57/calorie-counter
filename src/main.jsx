ae0d83ebe976fb22df5e0df51a434fca3987b68b
akashanand57 <akashanand330@gmail.com>
2024-12-29 15:53:08 +0530

    Code

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import App from './App';
-import './index.css';
-import VantaBackground from './components/VantaBackground';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import App from "./App";
+import "./index.css";
+import VantaBackground from "./components/VantaBackground";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <div className="vanta-wrapper">
+    <div className="vanta-wrapper" style={{ position: "relative", height: "100vh" }}>
+      {/* Vanta Background Component */}
       <VantaBackground />
-      <div className="app-wrapper">
+
+      {/* App Wrapper */}
+      <div
+        className="app-wrapper"
+        style={{
+          position: "relative",
+          zIndex: 1, // Ensures App content is layered above the Vanta background
+        }}
+      >
         <App />
       </div>
     </div>
